dab4b9bc0517f01637595636436ab6fff9cf9805
NAME <email adress>
2024-11-19 17:49:10 +0900

    Initial commit for first branch

---
+++ src/test/java/ftt_backend/FttBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ftt_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FttBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
