bee104502cc56136855ec85964068a6d2657d8d3
SungWookkk <patksy68@gmail.com>
2025-03-27 20:50:15 +0900

    3/27 feat: 뱃지 관련 로직들 추가
    - 사용자에게 뱃지 취득을 Test하기 위한 updateCompletedCount 메소드 추가
    - 사용자가 지금까지 취득한 뱃지를 수정하기 위한 activateUserBadge 추가
    - DataLoader 가입하기 편하게 하기 위한 설정 변경
    - UserBadge - activeBadgeId Column 추가
    >> 사용자가 선택한 뱃지의 반영을 위한 컬럼

--- src/main/java/ftt_backend/model/UserBadge.java
+++ src/main/java/ftt_backend/model/UserBadge.java
@@ -4,12 +4,16 @@ import ftt_backend.model.Badge;
 import ftt_backend.model.UserInfo;
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDate;
 
 @Entity
 @Table(name = "user_badge")
 @Data
+@Getter
+@Setter
 public class UserBadge {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -28,6 +32,10 @@ public class UserBadge {
     // 언제 획득했는지 기록
     @Column(name = "acquired_date")
     private LocalDate acquiredDate;
+
+    // 활성 상태의 뱃지인지 식별하기 위한 칼럼
+    @Column(name = "active_badge_id", nullable = true)
+    private Long activeBadgeId;
 
     // 뱃지 활성/비활성 여부, 등등 필요한 컬럼 추가 가능
     // @Column(name = "active")
