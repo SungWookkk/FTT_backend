5ffe9ff66c803719525002d6de5e85574d6edae6
SungWookkk <patksy68@gmail.com>
2025-04-03 11:44:32 +0900

    4/3 feat: [팀] FE axios 생성 조회 로직 처리
    - [Controller] @RequestBody 팀 생성 요청 처리
    - [Service] 팀 생성시 생성자 또한 팀 리더와 팀원 동시 적용 및 소속 팀 렌더링을 위한 userId 조회
    - [Repository] ManyToMany members id가 userId인 Team 목록 조회

--- src/main/java/ftt_backend/controller/TeamController.java
+++ src/main/java/ftt_backend/controller/TeamController.java
@@ -1,4 +1,30 @@
 package ftt_backend.controller;
 
+import ftt_backend.model.Team;
+import ftt_backend.service.TeamService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/teams")
 public class TeamController {
+
+    @Autowired
+    private TeamService teamService;
+
+    // 팀 생성 요청 처리
+    @PostMapping("/create")
+    public ResponseEntity<Team> createTeam(@RequestBody Team team) {
+        Team createdTeam = teamService.createTeam(team);
+        return new ResponseEntity<>(createdTeam, HttpStatus.CREATED);
+    }
+    // 사용자(userId)가 속한 팀 목록 조회
+    @GetMapping("/user/{userId}")
+    public List<Team> getTeamsByUserId(@PathVariable Long userId) {
+        return teamService.findTeamsByUserId(userId);
+    }
 }
