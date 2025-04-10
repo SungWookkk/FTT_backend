e8406971eb78b1f9d98602d9b1d0c7f994059a96
SungWookkk <patksy68@gmail.com>
2025-04-10 16:24:47 +0900

    4/10 feat: [팀] 팀 페이지 접속 후 생긴 오류 해결
    - members가 빈 배열로 들어가 생긴  .filter() 오류를 해결

--- src/main/java/ftt_backend/controller/TeamController.java
+++ src/main/java/ftt_backend/controller/TeamController.java
@@ -16,17 +16,23 @@ public class TeamController {
     @Autowired
     private TeamService teamService;
 
-    // 팀 생성 요청 처리
+    // 팀 생성 요청 처리 (헤더에서 팀 생성자의 아이디(X-User-Id) 를 받음)
     @PostMapping("/create")
-    public ResponseEntity<Team> createTeam(@RequestBody Team team) {
-        Team createdTeam = teamService.createTeam(team);
+    public ResponseEntity<Team> createTeam(
+            @RequestHeader("X-User-Id") Long userId,
+            @RequestBody Team team
+            // 요청 본문에 팀 객체만 보내도 되고 추가로 헤더에 팀 생성자의 아이디를 보냄
+    ) {
+        Team createdTeam = teamService.createTeam(team, userId);
         return new ResponseEntity<>(createdTeam, HttpStatus.CREATED);
     }
+
     // 사용자(userId)가 속한 팀 목록 조회
     @GetMapping("/user/{userId}")
     public List<Team> getTeamsByUserId(@PathVariable Long userId) {
         return teamService.findTeamsByUserId(userId);
     }
+
     // 팀 상세 조회
     @GetMapping("/{teamId}")
     public ResponseEntity<Team> getTeamById(@PathVariable Long teamId) {
